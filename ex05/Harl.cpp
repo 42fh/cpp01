3985349c5b4ccebe59533a35d10e28f182d3426f
fheld <fheld@student.42berlin.de>
2023-11-05 00:30:12 +0100

    remvoed map and replaced it with for loop

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,9 +1,17 @@
 #include "Harl.h"
 
-Harl::Harl(/* args */)
-{
-}
+Harl::Harl(){}
+Harl::~Harl(){}
+void Harl::debug( void ){std::cout << "I love .. (debug)" << std::endl;}
+void Harl::info( void ){std::cout << "I cannot.. (info)" << std::endl;}
+void Harl::warning( void ){std::cout << "I think I.. (warning)" << std::endl;}
+void Harl::error( void ){std::cout << "This is un.. (error)" << std::endl;}
 
-Harl::~Harl()
+void Harl::complain( std::string level )
 {
+	typedef void (Harl::*fptr)();
+	fptr parray[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string sarray[4] = {"debug", "info", "warning", "error"};
+	
+	for (size_t i = 0; i < 4; i++){if (level == sarray[i]) (this->*parray[i])();}
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,10 +1,11 @@
-#include <map>
+#include <iostream>
+#include <cstdlib>
 #include "Harl.h"
 
 // typedef void(*fpt)(void);
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::map<std::string, void(*)(void)> myMap;
-	return 0;
+	if (argc != 2){std::cerr << "must supply one argument" << std::endl; std::exit(1);}
+	Harl h; std::string s(argv[1]); h.complain(s); return 0;
 }
