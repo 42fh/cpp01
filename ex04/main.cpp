878e5d76b2401ccecb8947e9b73924c506d0fd17
fheld <fheld@student.42berlin.de>
2023-11-04 19:08:31 +0100

    finished ex04 by converting to stringstream to string and writing to file

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <sstream>
 
 int main(int argc, char const *argv[])
 {
 	if (argc != 4){std::cout << "Erro: input must be 3 arguments"; std::exit(1);}
-	(void) argv;
-	
-	std::ofstream output;
-	std::ifstream input;
-	
-	output.open("example.txt");
-	if(output.is_open() != true){std::cout << "Error: can't open file"; std::exit(1);}
-	
-	
 
-	output << "Writing this to a file.\n";
+	std::string filename(argv[1]);
+	std::string findstr(argv[2]);	
+	std::string replstr(argv[3]);	
+
+	std::ifstream input;
+	std::ofstream output;
 	
+	input.open(filename.c_str());
+	output.open((filename + ".replace").c_str());
+
+	// check if opening failed
+	if(input.is_open() != true){std::cout << "Error: can't open file"; std::exit(1);}
+	if(output.is_open() != true){std::cout << "Error: can't open file"; input.close(); std::exit(1);}
+	
+	// convert file to stringstream to buffer
+	std::stringstream ss;
+	ss << input.rdbuf();
+	std::string instr(ss.str());
+	size_t pos(0);
+
+	// loop through string
+	while ((pos = instr.find(findstr, pos)) != std::string::npos)
+	{
+		instr.erase(pos, findstr.length());
+		instr.insert(pos, replstr);
+		pos += replstr.length();
+	}
+	output << instr;
+	input.close();
 	output.close();
 	return 0;
 }
